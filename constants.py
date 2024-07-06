b72b39b61593a7ce4790bdb8c32377e5a8bb390b
RPINerd <RPINerd@gmail.com>
2024-07-05 22:15:22 -0400

    Transitioned to using enums to better align with source code

--- constants.py
+++ constants.py
@@ -12,6 +12,32 @@
     https://github.com/blind-coder/SpaceTrader/tree/master/SpaceTrader/src/main/java/de/anderdonau/spacetrader/DataTypes
 """
 
+from enum import Enum
+
+COLORS = {
+    "default": "brown",
+    "limit": "red",
+    "homeworld": "blue",
+    "visited": "green",
+    "target": "grey",
+    "selected": "brown",
+    "background": "lightgrey",
+}
+OVERVIEW = [
+    "Space Trader is a complex game, in which the player's aim",
+    "is to amass enough money to be able to buy a moon to retire",
+    "to. The player starts out with a small space ship, armed",
+    "with one simple laser, and 1000 credits in cash. The safest",
+    "and easiest way to earn money is to trade goods between",
+    "neighbouring solar systems. If the player chooses the goods",
+    "to trade wisely, it isn't too difficult to sell them with",
+    "a profit. There are other ways to get rich, though. You",
+    "might become a bounty hunter and hunt down pirates. It is",
+    "also possible to become a pirate yourself and rob honest",
+    "traders of their cargo. Beware, though: pirating is a way",
+    "to get rich quickly, but the police force will go after you.",
+]
+
 # Galaxy
 GALAXYWIDTH = 154
 GALAXYHEIGHT = 110
@@ -28,415 +54,205 @@ DEBT_LIMIT = 100000
 MAX_NOCLAIM = 90  # TODO define
 START_CREDITS = 1000
 
+
 # Crime
-BOUNTYMOD = 1000  # Price paid by govt for each neg police point
-POLICE_RECORDS = {
-    -100: "Psychopath",
-    -70: "Villain",
-    -30: "Criminal",
-    -10: "Crook",
-    -5: "Dubious",
-    0: "Clean",
-    5: "Lawful",
-    10: "Trusted",
-    25: "Liked",
-    75: "Hero",
-    100: "ErrNo",
-}
+class CriminalRecord(Enum):
+    PSYCHOPATH = 0
+    VILLAIN = 1
+    CRIMINAL = 2
+    CROOK = 3
+    DUBIOUS = 4
+    CLEAN = 5
+    LAWFUL = 6
+    TRUSTED = 7
+    LIKED = 8
+    HERO = 9
+    ERRNO = 10
+
 
 # Combat
-REPUTATION = {
-    0: "Harmless",
-    10: "Mostly Harmless",
-    20: "Poor",
-    40: "Average",
-    80: "Above Average",
-    150: "Competent",
-    300: "Dangerous",
-    600: "Deadly",
-    1500: "Elite",
-    3000: "Borg",
-}
+class CombatReputation(Enum):
+    HARMLESS = 0
+    MOSTLY_HARMLESS = 1
+    POOR = 2
+    AVERAGE = 3
+    ABOVE_AVERAGE = 4
+    COMPETENT = 5
+    DANGEROUS = 6
+    DEADLY = 7
+    ELITE = 8
+    BORG = 9
 
-COLORS = {
-    "default": "brown",
-    "limit": "red",
-    "homeworld": "blue",
-    "visited": "green",
-    "target": "grey",
-    "selected": "brown",
-    "background": "lightgrey",
-}
-
-OVERVIEW = [
-    "Space Trader is a complex game, in which the player's aim",
-    "is to amass enough money to be able to buy a moon to retire",
-    "to. The player starts out with a small space ship, armed",
-    "with one simple laser, and 1000 credits in cash. The safest",
-    "and easiest way to earn money is to trade goods between",
-    "neighbouring solar systems. If the player chooses the goods",
-    "to trade wisely, it isn't too difficult to sell them with",
-    "a profit. There are other ways to get rich, though. You",
-    "might become a bounty hunter and hunt down pirates. It is",
-    "also possible to become a pirate yourself and rob honest",
-    "traders of their cargo. Beware, though: pirating is a way",
-    "to get rich quickly, but the police force will go after you.",
-]
 
 # System
-ACTIVITY = {0: "Absent", 1: "Minimal", 2: "Few", 3: "Some", 4: "Moderate", 5: "Abundant", 6: "Many", 7: "Swarms"}
-SYSTEM_NAMES = [
-    "Acamar",
-    "Adahn",
-    "Aldea",
-    "Andevian",
-    "Antedi",
-    "Balosnee",
-    "Baratas",
-    "Bob",
-    "Brax",
-    "Bretel",
-    "Calondia",
-    "Campor",
-    "Capelle",
-    "Carzon",
-    "Castor",
-    "Cestus",
-    "Cheron",
-    "Courteney",
-    "Daled",
-    "Damast",
-    "Davlos",
-    "Deneb",
-    "Deneva",
-    "Devidia",
-    "Draylon",
-    "Drema",
-    "Endor",
-    "Esmee",
-    "Exo",
-    "Ferris",
-    "Festen",
-    "Fourmi",
-    "Frolix",
-    "Gemulon",
-    "Guinifer",
-    "Hades",
-    "Hamlet",
-    "Helena",
-    "Hulst",
-    "Iodine",
-    "Iralius",
-    "Janus",
-    "Japori",
-    "Jarada",
-    "Jason",
-    "Kaylon",
-    "Khefka",
-    "Kira",
-    "Klaatu",
-    "Klaestron",
-    "Korma",
-    "Kravat",
-    "Krios",
-    "Laertes",
-    "Largo",
-    "Lave",
-    "Ligon",
-    "Lowry",
-    "Magrat",
-    "Malcoria",
-    "Melina",
-    "Mentar",
-    "Merik",
-    "Mintaka",
-    "Montor",
-    "Mordan",
-    "Myrthe",
-    "Nelvana",
-    "Nix",
-    "Nyle",
-    "Odet",
-    "Og",
-    "Omega",
-    "Omphalos",
-    "Orias",
-    "Othello",
-    "Parade",
-    "Penthara",
-    "Picard",
-    "Pollux",
-    "Quator",
-    "Rakhar",
-    "Ran",
-    "Regulas",
-    "Relva",
-    "Rhymus",
-    "Rochani",
-    "Rubicum",
-    "Rutia",
-    "Sarpeidon",
-    "Sefalla",
-    "Seltrice",
-    "Sigma",
-    "Sol",
-    "Somari",
-    "Stakoron",
-    "Styris",
-    "Talani",
-    "Tamus",
-    "Tantalos",
-    "Tanuga",
-    "Tarchannen",
-    "Terosa",
-    "Thera",
-    "Titan",
-    "Torin",
-    "Triacus",
-    "Turkana",
-    "Tyrus",
-    "Umberlee",
-    "Utopia",
-    "Vadera",
-    "Vagra",
-    "Vandor",
-    "Ventax",
-    "Xenon",
-    "Xerxes",
-    "Yew",
-    "Yojimbo",
-    "Zalkon",
-    "Zuul",
-]
-GOVERNMENT = {
-    0: "Anarchy",
-    1: "Capitalist",
-    2: "Communist",
-    3: "Confederacy",
-    4: "Corporate",
-    5: "Cybernetic",
-    6: "Democracy",
-    7: "Dictatorship",
-    8: "Fascist",
-    9: "Feudal",
-    10: "Military",
-    11: "Monarchy",
-    12: "Pacifist",
-    13: "Socialist",
-    14: "Satori",
-    15: "Technocracy",
-    16: "Theocracy",
+class Activity(Enum):
+    ABSENT = 0
+    MINIMAL = 1
+    FEW = 2
+    SOME = 3
+    MODERATE = 4
+    MANY = 5
+    ABUNDANT = 6
+    SWARMS = 7
+    UNAVAILABLE = 100
+
+
+class Government(Enum):
+    ANARCHY = 0
+    CAPITALIST = 1
+    COMMUNIST = 2
+    CONFEDERACY = 3
+    CORPORATE = 4
+    CYBERNETIC = 5
+    DEMOCRACY = 6
+    DICTATORSHIP = 7
+    FASCIST = 8
+    FEUDAL = 9
+    MILITARY = 10
+    MONARCHY = 11
+    PACIFIST = 12
+    SOCIALIST = 13
+    SATORI = 14
+    TECHNOCRACY = 15
+    THEOCRACY = 16
+
+
+class TechLevel(Enum):
+    PRE_AGRICULTURAL = 0
+    AGRICULTURAL = 1
+    MEDIEVAL = 2
+    RENAISSANCE = 3
+    EARLY_INDUSTRIAL = 4
+    INDUSTRIAL = 5
+    POST_INDUSTRIAL = 6
+    HI_TECH = 7
+    UNAVAILABLE = 8
+
+
+class Size(Enum):
+    TINY = 0
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
+    HUGE = 4
+    GARGANTUAN = 5
+
+
+class SpecialResource(Enum):
+    NOTHING = 0
+    MINERAL_RICH = 1
+    MINERAL_POOR = 2
+    DESERT = 3
+    SWEETOCEANS = 4
+    RICHSOIL = 5
+    POORSOIL = 6
+    RICHFAUNA = 7
+    LIFELESS = 8
+    WEIRDMUSHROOMS = 9
+    SPECIALHERBS = 10
+    ARTISTIC = 11
+    WARLIKE = 12
+
+
+class SystemPressure(Enum):
+    NONE = 0
+    WAR = 1
+    PLAGUE = 2
+    DROUGHT = 3
+    BOREDOM = 4
+    COLD = 5
+    CROPFAILURE = 6
+    EMPLOYMENT = 7
+
+
+SYSTEMSIZE = {
+    SystemSize.TINY: "Tiny",
+    SystemSize.SMALL: "Small",
+    SystemSize.MEDIUM: "Medium",
+    SystemSize.LARGE: "Large",
+    SystemSize.HUGE: "Huge",
+    SystemSize.GARGANTUAN: "Gargantuan",
 }
-TECHLEVEL = {
-    0: "Pre-agricultural",
-    1: "Agricultural",
-    2: "Medieval",
-    3: "Renaissance",
-    4: "Early Industrial",
-    5: "Industrial",
-    6: "Post-industrial",
-    7: "Hi-tech",
-}
-SYSTEMSIZE = {0: "Tiny", 1: "Small", 2: "Medium", 3: "Large", 4: "Huge"}
 SPECIALRESOURCES = {
-    "nothing": "Nothing special",  # Uneventful
-    "mineralrich": "Mineral rich",  # produce Ore
-    "mineralpoor": "Mineral poor",  # Ore in demand
-    "desert": "Desert",  # Water in demand
-    "lotsofwater": "Sweetwater oceans",  # produce Water
-    "soilrich": "Rich soil",  # produce Food
-    "soilpoor": "Poor soil",  # Food in demand
-    "faunarich": "Rich fauna",  # produce Fur
-    "lifepoor": "Lifeless",  # Water and Food in demand
-    "weirdmushrooms": "Weird mushrooms",  # produce Narcotics
-    "lotsofherbs": "Special herbs",  # produce Narcotics
-    "artistic": "Artistic populace",  # Narcotics in demand
-    "warlike": "Warlike populace",
-}  # Weapons in demand
-STATUS = {
-    "nopressure": "under no particular pressure",  # Uneventful
-    "war": "at war",  # Ore and Weapons in demand
-    "plague": "ravaged by a plague",  # Medicine in demand
-    "drought": "suffering from a drought",  # Water in demand
-    "boredom": "suffering from extreme boredom",  # Games and Narcotics in demand
-    "cold": "suffering from a cold spell",  # Furs in demand
-    "cropfailure": "suffering from a crop failure",  # Food in demand
-    "lackofworkers": "lacking enough workers",
+    SpecialResource.NOTHING: "Nothing special",  # Uneventful
+    SpecialResource.MINERAL_RICH: "Mineral rich",  # produce Ore
+    SpecialResource.MINERAL_POOR: "Mineral poor",  # Ore in demand
+    SpecialResource.DESERT: "Desert",  # Water in demand
+    SpecialResource.SWEETOCEANS: "Sweetwater oceans",  # produce Water
+    SpecialResource.RICHSOIL: "Rich soil",  # produce Food
+    SpecialResource.POORSOIL: "Poor soil",  # Food in demand
+    SpecialResource.RICHFAUNA: "Rich fauna",  # produce Fur
+    SpecialResource.LIFELESS: "Lifeless",  # Water and Food in demand
+    SpecialResource.WEIRDMUSHROOMS: "Weird mushrooms",  # produce Narcotics
+    SpecialResource.SPECIALHERBS: "Special herbs",  # produce Narcotics
+    SpecialResource.ARTISTIC: "Artistic populace",  # Narcotics in demand
+    SpecialResource.WARLIKE: "Warlike populace",  # Weapons in demand
+}
+PRESSURE = {
+    SystemPressure.NONE: "under no particular pressure",  # Uneventful
+    SystemPressure.WAR: "at war",  # Ore and Weapons in demand
+    SystemPressure.PLAGUE: "ravaged by a plague",  # Medicine in demand
+    SystemPressure.DROUGHT: "suffering from a drought",  # Water in demand
+    SystemPressure.BOREDOM: "suffering from extreme boredom",  # Games and Narcotics in demand
+    SystemPressure.COLD: "suffering from a cold spell",  # Furs in demand
+    SystemPressure.CROPFAILURE: "suffering from a crop failure",  # Food in demand
+    SystemPressure.EMPLOYMENT: "lacking enough workers",
 }  # Machinery and Robots in demand
 
-"""
-class TradeItem/Goods:
-Tradeitem("Water", 0, 0, 2, 30, +3, 4, GameState.DROUGHT, GameState.LOTSOFWATER, GameState.DESERT, 30, 50, 1)
-    key: name;
-    tp: techProduction          // Tech level needed for production
-    tu: techUsage               // Tech level needed to use
-    ttp: techTopProduction      // Tech level which produces this item the most
-    plt: priceLowTech           // Medium price at lowest tech level
-    pi: priceInc                // Price increase per tech level
-    var: variance               // Max percentage above or below calculated price
-    dps: doublePriceStatus      // Price increases considerably when this event occurs
-    cr: cheapResource           // When this resource is available, this trade item is cheap
-    er: expensiveResource       // When this resource is available, this trade item is expensive
-    mintp: minTradePrice        // Minimum price to buy/sell in orbit
-    maxtp: maxTradePrice        // Maximum price to buy/sell in orbit
-    ro: roundOff                // Roundoff price for trade in orbit
-"""
-GOODS = {
-    "water": {
-        "tp": 0,
-        "tu": 0,
-        "ttp": 2,
-        "plt": 30,
-        "pi": +3,
-        "var": 4,
-        "dps": "drought",
-        "cr": "lotsofwater",
-        "er": "desert",
-        "mintp": 30,
-        "maxtp": 50,
-        "ro": 1,
-    },
-    "furs": {
-        "tp": 0,
-        "tu": 0,
-        "ttp": 0,
-        "plt": 250,
-        "pi": +10,
-        "var": 10,
-        "dps": "cold",
-        "cr": "faunarich",
-        "er": "lifepoor",
-        "mintp": 230,
-        "maxtp": 280,
-        "ro": 5,
-    },
-    "food": {
-        "tp": 1,
-        "tu": 0,
-        "ttp": 1,
-        "plt": 100,
-        "pi": +5,
-        "var": 5,
-        "dps": "cropfailure",
-        "cr": "soilrich",
-        "er": "soilpoor",
-        "mintp": 90,
-        "maxtp": 160,
-        "ro": 5,
-    },
-    "ore": {
-        "tp": 2,
-        "tu": 2,
-        "ttp": 3,
-        "plt": 350,
-        "pi": +20,
-        "var": 10,
-        "dps": "war",
-        "cr": "mineralrich",
-        "er": "mineralpoor",
-        "mintp": 350,
-        "maxtp": 420,
-        "ro": 10,
-    },
-    "games": {
-        "tp": 3,
-        "tu": 1,
-        "ttp": 6,
-        "plt": 250,
-        "pi": -10,
-        "var": 5,
-        "dps": "boredom",
-        "cr": "artistic",
-        "er": None,
-        "mintp": 160,
-        "maxtp": 270,
-        "ro": 5,
-    },
-    "firearms": {
-        "tp": 3,
-        "tu": 1,
-        "ttp": 5,
-        "plt": 1250,
-        "pi": -75,
-        "var": 100,
-        "dps": "war",
-        "cr": "warlike",
-        "er": None,
-        "mintp": 600,
-        "maxtp": 1100,
-        "ro": 25,
-    },
-    "medicines": {
-        "tp": 4,
-        "tu": 1,
-        "ttp": 6,
-        "plt": 650,
-        "pi": -20,
-        "var": 10,
-        "dps": "plague",
-        "cr": "lotsofherbs",
-        "er": None,
-        "mintp": 400,
-        "maxtp": 700,
-        "ro": 25,
-    },
-    "machines": {
-        "tp": 4,
-        "tu": 3,
-        "ttp": 5,
-        "plt": 900,
-        "pi": -30,
-        "var": 5,
-        "dps": "lackofworkers",
-        "cr": None,
-        "er": None,
-        "mintp": 600,
-        "maxtp": 800,
-        "ro": 25,
-    },
-    "narcotics": {
-        "tp": 5,
-        "tu": 0,
-        "ttp": 5,
-        "plt": 3500,
-        "pi": -125,
-        "var": 150,
-        "dps": "boredom",
-        "cr": "weirdmushrooms",
-        "er": None,
-        "mintp": 2000,
-        "maxtp": 3000,
-        "ro": 50,
-    },
-    "robots": {
-        "tp": 6,
-        "tu": 4,
-        "ttp": 7,
-        "plt": 5000,
-        "pi": -150,
-        "var": 100,
-        "dps": "lackofworkers",
-        "cr": None,
-        "er": None,
-        "mintp": 3500,
-        "maxtp": 5000,
-        "ro": 100,
-    },
-    "fuel": {
-        "tp": 4,
-        "tu": 0,
-        "ttp": 7,
-        "plt": 17,
-        "pi": -1,
-        "var": 15,
-        "dps": "war",
-        "cr": "warlike",
-        "er": None,
-        "mintp": 17,
-        "maxtp": 5000,
-        "ro": 1,
-    },
-}
+
+# Equipment
+class EquipmentType(Enum):
+    WEAPON = 0
+    SHIELD = 1
+    GADGET = 2
+
+
+class GadgetID(Enum):
+    CARGOBAYS = 0
+    AUTOREPAIR = 1
+    NAVIGATION = 2
+    TARGETING = 3
+    CLOAKING = 4
+    FUELCOMPACTOR = 5
+    SMUGGLERHOLD = 6
+
+
+class WeaponID(Enum):
+    PULSELASER = 0
+    BEAMLASER = 1
+    MILITARYLASER = 2
+    MORGANSLASER = 3
+    PHOTONDISRUPTOR = 4
+    QUANTUMDISRUPTOR = 5
+
+
+class SheildID(Enum):
+    ENERGY = 0
+    REFLECTIVE = 1
+    LIGHTNING = 2
+
+
+# Ships
+class ShipID(Enum):
+    # ESCAPEPOD = 0 #? Not in source
+    FLEA = 0
+    GNAT = 1
+    FIREFLY = 2
+    MOSQUITO = 3
+    BUMBLEBEE = 4
+    BEETLE = 5
+    HORNET = 6
+    GRASSHOPPER = 7
+    TERMITE = 8
+    WASP = 9
+    SPACEMONSTER = 10
+    DRAGONFLY = 11
+    MANTIS = 12
+    SCARAB = 13
+    BOTTLE = 14
+    CUSTOM = 15
+    SCORPION = 16
+
 
 SHIPTYPES = {
     "escapepod": {
@@ -634,15 +450,131 @@ SHIPTYPES = {
     },
 }
 
-# TODO/FIXME best to separate?
-EQUIPEMENTS = {
-    "weapons": {"pulse": 1, "beam": 2, "military": 3},
-    "shields": {"energy": 1, "reflective": 2, "lightning": 3},
-    "gadgets": {"autorepair": 1, "navigating": 1, "targeting": 1, "cloacking": 1, "escape": 1},
-    "compactor": 1,
-    "cargopod": 5,
-}
 
+# Names
+SYSTEM_NAMES = [
+    "Acamar",
+    "Adahn",
+    "Aldea",
+    "Andevian",
+    "Antedi",
+    "Balosnee",
+    "Baratas",
+    "Bob",
+    "Brax",
+    "Bretel",
+    "Calondia",
+    "Campor",
+    "Capelle",
+    "Carzon",
+    "Castor",
+    "Cestus",
+    "Cheron",
+    "Courteney",
+    "Daled",
+    "Damast",
+    "Davlos",
+    "Deneb",
+    "Deneva",
+    "Devidia",
+    "Draylon",
+    "Drema",
+    "Endor",
+    "Esmee",
+    "Exo",
+    "Ferris",
+    "Festen",
+    "Fourmi",
+    "Frolix",
+    "Gemulon",
+    "Guinifer",
+    "Hades",
+    "Hamlet",
+    "Helena",
+    "Hulst",
+    "Iodine",
+    "Iralius",
+    "Janus",
+    "Japori",
+    "Jarada",
+    "Jason",
+    "Kaylon",
+    "Khefka",
+    "Kira",
+    "Klaatu",
+    "Klaestron",
+    "Korma",
+    "Kravat",
+    "Krios",
+    "Laertes",
+    "Largo",
+    "Lave",
+    "Ligon",
+    "Lowry",
+    "Magrat",
+    "Malcoria",
+    "Melina",
+    "Mentar",
+    "Merik",
+    "Mintaka",
+    "Montor",
+    "Mordan",
+    "Myrthe",
+    "Nelvana",
+    "Nix",
+    "Nyle",
+    "Odet",
+    "Og",
+    "Omega",
+    "Omphalos",
+    "Orias",
+    "Othello",
+    "Parade",
+    "Penthara",
+    "Picard",
+    "Pollux",
+    "Quator",
+    "Rakhar",
+    "Ran",
+    "Regulas",
+    "Relva",
+    "Rhymus",
+    "Rochani",
+    "Rubicum",
+    "Rutia",
+    "Sarpeidon",
+    "Sefalla",
+    "Seltrice",
+    "Sigma",
+    "Sol",
+    "Somari",
+    "Stakoron",
+    "Styris",
+    "Talani",
+    "Tamus",
+    "Tantalos",
+    "Tanuga",
+    "Tarchannen",
+    "Terosa",
+    "Thera",
+    "Titan",
+    "Torin",
+    "Triacus",
+    "Turkana",
+    "Tyrus",
+    "Umberlee",
+    "Utopia",
+    "Vadera",
+    "Vagra",
+    "Vandor",
+    "Ventax",
+    "Xenon",
+    "Xerxes",
+    "Yew",
+    "Yojimbo",
+    "Zalkon",
+    "Zuul",
+]
 MERCENARYNAMES = [
     "Alyssa",
     "Armatur",
@@ -675,3 +607,69 @@ MERCENARYNAMES = [
     "Yorvick",
     "Zeethibal",  # anagram of Elizabeth
 ]
+
+
+# Character
+class Skills(Enum):
+    NONE = 0
+    PILOT = 1
+    FIGHTER = 2
+    TRADER = 3
+    ENGINEER = 4
+
+
+class Difficulty(Enum):
+    BEGINNER = 0
+    EASY = 1
+    NORMAL = 2
+    HARD = 3
+    IMPOSSIBLE = 4
+
+
+MAXTRIBBLES = 100000
+MAXSKILL = 10
+SKILLBONUS = 3
+CLOAKBONUS = 2
+BOUNTYMOD = 1000  # Price paid by govt for each neg police point
+POLICE_RECORDS = {
+    CriminalRecord.PSYCHOPATH: -100,
+    CriminalRecord.VILLAIN: -70,
+    CriminalRecord.CRIMINAL: -30,
+    CriminalRecord.CROOK: -10,
+    CriminalRecord.DUBIOUS: -5,
+    CriminalRecord.CLEAN: 0,
+    CriminalRecord.LAWFUL: 5,
+    CriminalRecord.TRUSTED: 10,
+    CriminalRecord.LIKED: 25,
+    CriminalRecord.HERO: 75,
+    CriminalRecord.ERRNO: 100,
+}
+REPUTATION = {
+    CombatReputation.HARMLESS: 0,
+    CombatReputation.MOSTLY_HARMLESS: 10,
+    CombatReputation.POOR: 20,
+    CombatReputation.AVERAGE: 40,
+    CombatReputation.ABOVE_AVERAGE: 80,
+    CombatReputation.COMPETENT: 150,
+    CombatReputation.DANGEROUS: 300,
+    CombatReputation.DEADLY: 600,
+    CombatReputation.ELITE: 1500,
+    CombatReputation.BORG: 3000,
+}
+
+
+# TODO Unknown values/usage
+SCOREATTACKPIRATE = 0
+SCOREATTACKPOLICE = -3
+SCOREATTACKTRADER = -2
+SCORECAUGHTWITHWILD = -4
+SCOREFLEEPOLICE = -2
+SCOREKILLCAPTAIN = 100
+SCOREKILLPIRATE = 1
+SCOREKILLPOLICE = -6
+SCOREKILLTRADER = -4
+SCOREPLUNDERPIRATE = -1
+SCOREPLUNDERTRADER = -2
+SCORETRAFFICKING = -1
+
+SHIPTEMPLATESEPARATOR = "----------------------------"
