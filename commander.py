6f600286f808bd0194c536d7fe0ca532db762f5b
RPINerd <RPINerd@gmail.com>
2024-07-05 22:15:58 -0400

    Added skill management functions, enum imports

--- commander.py
+++ commander.py
@@ -1,4 +1,4 @@
-import constants as const
+from constants import INSURANCE_RATE, INTEREST_RATE, Skills
 
 
 class Commander:
@@ -25,10 +25,39 @@ class Commander:
 
         debt_interest = 0
         if self.debt > 0:
-            debt_interest = max(1, self.debt * const.INTEREST_RATE)
+            debt_interest = max(1, self.debt * INTEREST_RATE)
             if self.credits > debt_interest:
                 self.credits -= debt_interest
             else:
                 self.debt += debt_interest - self.credits
                 self.credits = 0
         self.debt = self.debt * 1.1
+
+    def get_debt(self):
+        return self.debt
+
+    def improve_skill(self, skill: int, improvement: int):
+        # TODO add skill cap
+        if skill == Skills.PILOT:
+            self.pilotSkill += improvement
+        elif skill == Skills.FIGHTER:
+            self.fighterSkill += improvement
+        elif skill == Skills.TRADER:
+            self.traderSkill += improvement
+        elif skill == Skills.ENGINEER:
+            self.engineerSkill += improvement
+        else:
+            raise ValueError(f"Invalid skill type, expected [0-3] but got {skill}!")
+
+    def deteriorate_skill(self, skill: int, deterioration: int):
+        # TODO prevent from going below 1
+        if skill == Skills.PILOT:
+            self.pilotSkill -= deterioration
+        elif skill == Skills.FIGHTER:
+            self.fighterSkill -= deterioration
+        elif skill == Skills.TRADER:
+            self.traderSkill -= deterioration
+        elif skill == Skills.ENGINEER:
+            self.engineerSkill -= deterioration
+        else:
+            raise ValueError(f"Invalid skill type, expected [0-3] but got {skill}!")
