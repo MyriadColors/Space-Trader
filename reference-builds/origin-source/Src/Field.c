13c0a78067f91a6b3b663fd4e1ac7805d70a8f74
RPINerd <RPINerd@gmail.com>
2024-07-17 22:45:15 -0400

    Tired of scrolling when opening files

--- reference-builds/origin-source/Src/Field.c
+++ reference-builds/origin-source/Src/Field.c
@@ -1,47 +1,13 @@
-/***********************************************************************
- *
- * SPACE TRADER 1.2.0
- *
+/*
  * Field.c
- *
- * Copyright (C) 2000-2002 Pieter Spronck, All Rights Reserved
- *
- * Additional coding by Sam Anderson (rulez2@home.com)
- * Additional coding by Samuel Goldstein (palm@fogbound.net)
- *
- * Some code of Matt Lee's Dope Wars program has been used.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
- * You can contact the author at space_trader@hotmail.com
- *
- * For those who are familiar with the classic game Elite: many of the
- * ideas in Space Trader are heavily inspired by Elite.
- *
- **********************************************************************/
+ */
 
-// *************************************************************************
 // Field.c - Functions Include:
 // Handle SetField( FormPtr frm, int Nr, char* Value, int Size, Boolean Focus )
 // void GetField( FormPtr frm, int Nr, char* Value, Handle AmountH )
 // void SetCheckBox( FormPtr frm, int Nr, Boolean Value )
 // Boolean GetCheckBox( FormPtr frm, int Nr )
-//
-// Modifications:
-// mm/dd/yy - description - author
-// *************************************************************************
+
 
 #include "external.h"
 
