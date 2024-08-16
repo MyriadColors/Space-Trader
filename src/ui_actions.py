16beeaef2d7044d4817e0f2040fc6d52eeaacf67
Chris <Rpinerd@gmail.com>
2024-08-16 15:43:09 -0400

    Some brainstorming on the actions to decouple from the UI

---
+++ src/ui_actions.py
@@ -0,0 +1,78 @@
+"""
+    Space Trader (PalmOS) | RPINerd, 2024
+
+    The thought with this file is to have a universal file that contains all the
+    actions that the user can take in the game.
+    In theory we can then just tie any UI into the functions of the game and not
+    have to worry about rewriting the game logic.
+"""
+
+
+# Disordered list of user interactions
+def buy_fuel():
+    pass
+
+
+def buy_news():
+    pass
+
+
+def warp():
+    pass
+
+
+def attack():
+    pass
+
+
+def flee():
+    pass
+
+
+def ignore():
+    pass
+
+
+def orbit_trade():
+    """This is when a trader encounter requests to trade with the player"""
+    pass
+
+
+def repair():
+    pass
+
+
+def buy_good():
+    pass
+
+
+def sell_good():
+    pass
+
+
+def buy_ship():
+    pass
+
+
+def buy_equipment():
+    pass
+
+
+def sell_equipment():
+    pass
+
+
+def hire_crew():
+    pass
+
+
+def fire_crew():
+    pass
+
+
+def buy_insurance():
+    pass
+
+
+def get_loan():
+    pass
